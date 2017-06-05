3c16a4da9f236dc77541a472b7d6ba5d4b9eb06b
Lukas Boersma <lukas.boersma@rwth-aachen.de>
2017-06-05 04:53:19 -0700

    Update documentation

--- FastCGI/FCGIApplication.cs
+++ FastCGI/FCGIApplication.cs
@@ -43,10 +43,17 @@ namespace FastCGI
     /// var app = new FCGIApplication();
     ///
     /// // Handle requests by responding with a 'Hello World' message
-    /// app.OnRequestReceived += (sender, request) => {
-    ///     request.WriteResponseASCII("HTTP/1.1 200 OK\nContent-Type:text/html\n\nHello World!");
-    ///     request.Close();
-    /// };
+    /// app.OnRequestReceived += (sender, request) =>
+    ///     {
+    ///         var responseString =
+    ///             "HTTP/1.1 200 OK\n"
+    ///             + "Content-Type:text/html\n"
+    ///             + "\n"
+    ///             + "Hello World!";
+    /// 
+    ///         request.WriteResponseASCII(responseString);
+    ///         request.Close();
+    ///     };
     /// // Start listening on port 19000
     /// app.Run(19000);
     /// 
